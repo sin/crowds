5821e74f113e28b418e4c08e7dabcc9488c477b6
Mateusz Sienkan <sin.mateusz@gmail.com>
2018-08-17 16:30:24 +0200

    Move the 'play' button on titlepage 30px down (refs #77)

--- js/chapters/A_Preloader.js
+++ js/chapters/A_Preloader.js
@@ -33,7 +33,7 @@ SLIDES.push(
 		{
 			type:"box",
 			id:"button",
-			text:"preloader_button", x:180, y:355, w:600, h:100, align:"center"
+			text:"preloader_button", x:180, y:385, w:600, h:100, align:"center"
 		}
 
 	],
